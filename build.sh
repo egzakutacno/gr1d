85402498f056bdef5fe674ba609a2ab6f7b3f383
egzakutacno <chakyagm@gmail.com>
2025-06-27 08:16:45 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create Dockerfile
+cat > Dockerfile <<EOF
+FROM eniocarboni/docker-ubuntu-systemd:jammy
+
+RUN apt-get update && \\
+    apt-get install -y docker.io && \\
+    apt-get clean
+EOF
+
+# Build the docker image
+docker build -t my-ubuntu-systemd-with-docker .
